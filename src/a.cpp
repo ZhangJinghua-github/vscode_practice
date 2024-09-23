4827ce1501b127dfd48f61d35086f2418055c27d
ZhangJinghua <2024212168@bupt.edu.cn>
2024-09-23 13:23:04 +0800

    update

---
+++ src/a.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void print10()
+{
+    std::cout << 10 << std::endl;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include<cstdio>
+#include"a.h"
 int main()
 {
     printf("hello world");
+    print10();
     return 0;
 }
